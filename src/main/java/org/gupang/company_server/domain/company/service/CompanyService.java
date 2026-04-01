3a64c879b80789bf8275442d5e7fda34fe39c94b
kwanhyoung <kimgh961021@gmail.com>
2026-04-01 15:21:20 +0900

    feat : 업체 관리 비지니스 로직 추가

---
+++ src/main/java/org/gupang/company_server/domain/company/service/CompanyService.java
@@ -0,0 +1,64 @@
+package org.gupang.company_server.domain.company.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.gupang.common.exception.CustomException;
+import org.gupang.company_server.domain.company.model.Company;
+import org.gupang.company_server.domain.company.repository.CompanyRepository;
+import org.gupang.company_server.exception.ErrorCode;
+import org.gupang.company_server.presentation.dto.CompanyRequestDto;
+import org.gupang.company_server.presentation.dto.CompanyResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CompanyService {
+    private final CompanyRepository companyRepository;
+
+    // 업체등록
+    @Transactional
+    public UUID createCompany(CompanyRequestDto requestDto){
+        Company company = Company.builder()
+                .name(requestDto.name())
+                .address(requestDto.address())
+                .addressDetail(requestDto.addressDetail())
+                .hubId(requestDto.hubId())
+                .managerId(requestDto.managerId())
+                .build();
+        return companyRepository.save(company).getId();
+    }
+
+    // 업체 수정
+    @Transactional
+    public void updateCompany(UUID id, CompanyRequestDto requestDto){
+        Company company = companyRepository.findById(id)
+                .orElseThrow(()-> new CustomException(ErrorCode.COMPANY_NOT_FOUND));
+        company.update(requestDto.name(), requestDto.address(),requestDto.addressDetail());
+    }
+
+    // 업체 상세 조회
+    public CompanyResponseDto getCompany(UUID id){
+        return companyRepository.findById(id)
+                .map(CompanyResponseDto::from)
+                .orElseThrow(()-> new CustomException(ErrorCode.COMPANY_NOT_FOUND));
+    }
+
+    // 업체 전체 조회
+    public Page<CompanyResponseDto> getCompanies(Pageable pageable){
+        return companyRepository.findAllByIsDeletedFalse(pageable)
+                .map(CompanyResponseDto::from);
+    }
+
+    // 업체 삭제
+    @Transactional
+    public void deleteCompany(UUID id){
+        Company company = companyRepository.findById(id)
+                .orElseThrow(()-> new CustomException(ErrorCode.COMPANY_NOT_FOUND));
+        company.delete();
+    }
+}
