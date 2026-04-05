0053a44d17038ed1404c4f06ffbce4000015b6d5
kwanhyoung <kimgh961021@gmail.com>
2026-04-06 00:17:27 +0900

    refactor : 코드리뷰 반영 CompanyProviderImpl : 코드리뷰를 반영하여 데이터가 없는경우 null이 아닌 에러코드를 반영하도록 수정

--- src/main/java/org/gupang/company_server/product/infrastructure/provider/CompanyProviderImpl.java
+++ src/main/java/org/gupang/company_server/product/infrastructure/provider/CompanyProviderImpl.java
@@ -1,8 +1,10 @@
 package org.gupang.company_server.product.infrastructure.provider;
 
 import lombok.RequiredArgsConstructor;
+import org.gupang.common.exception.CustomException;
 import org.gupang.company_server.company.domain.Company;
 import org.gupang.company_server.company.domain.CompanyRepository;
+import org.gupang.company_server.exception.ErrorCode;
 import org.gupang.company_server.product.domain.service.CompanyData;
 import org.gupang.company_server.product.domain.service.CompanyProvider;
 import org.springframework.stereotype.Component;
@@ -17,7 +19,8 @@ public class CompanyProviderImpl implements CompanyProvider {
 
     @Override
     public CompanyData getCompany(UUID companyId) {
-        Company company = companyRepository.findById(companyId).orElse(null);
+        Company company = companyRepository.findById(companyId)
+                .orElseThrow(() -> new CustomException(ErrorCode.COMPANY_NOT_FOUND));
 
         return company == null ? null : new CompanyData(company.getId(), company.getName());
     }
